58467a43f29be809b822762e6aa3212363e96902
Alex Wilson <alex@cooperi.net>
2011-02-23 21:40:31 +1000

    Adding further json support

--- lib/utils/json.rb
+++ lib/utils/json.rb
@@ -96,6 +96,71 @@ module Rota
     end
   end
   
+  class TimetableEvent
+    def to_json(o, *opts)
+      o.date self.date
+      o.taught self.taught
+    end
+  end
+  
+  class TimetableSession
+    def to_json(o, *opts)
+      o.id(self['id'])
+      o.day(self.day)
+      o.start(self.start_time)
+      o.finish(self.finish_time)
+      o.startmins(self.start)
+      o.finishmins(self.finish)
+      o.room(self.room)
+      o.building do |b|
+        b.number(self.building.number)
+        b.name(self.building.name)
+      end
+      unless opts.include?(:no_children)
+        s.events(:array) do |a|
+          self.events.each do |ev|
+            a.object do |obj|
+              ev.to_json(obj)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  class TimetableGroup
+    def to_json(o, *opts)
+      o.id(self['id'])
+      o.name(self.name)
+      o.groupname(self.group_name)
+      unless opts.include?(:no_children)
+        o.sessions(:array) do |a|
+          self.sessions.each do |se|
+            a.object do |obj|
+              se.to_json(obj)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  class TimetableSeries
+    def to_json(o, *opts)
+      o.id(self['id'])
+      o.name(self.name)
+      unless opts.include?(:no_children)
+        o.groups(:array) do |g|
+          self.groups.each do |gg|
+            g.object do |obj|
+              gg.to_json(obj)
+            end
+          end
+        end
+      end
+    end
+  end
+  
   class Offering
     def to_json(o, *opts)
       o.id(self['id'])
