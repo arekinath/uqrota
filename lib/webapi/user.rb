4bbaaa2ebd71e82feb9215edb61a21902d71c7ac
Alex Wilson <nikosai@gmail.com>
2011-05-10 12:08:13 +1000

    Adding log in and log out API

--- lib/webapi/user.rb
+++ lib/webapi/user.rb
@@ -13,9 +13,12 @@ class UserService < Sinatra::Base
   mime_type :plain, 'text/plain'
   
   post '/login.json' do
-    user = User.get(params[:email])
+    user = Rota::User.get(params[:email])
     if not user.nil? and user.is_password?(params[:password])
       session[:user] = user
+      Utils.json { |j| j.success true }
+    else
+      Utils.json { |j| j.success false }
     end
   end
   
@@ -26,13 +29,12 @@ class UserService < Sinatra::Base
         j.email session[:user].email
       end
     else
-      Utils.json do |j|
-        j.logged_in false
-      end
+      Utils.json { |j| j.logged_in false }
     end
   end
   
   post '/logout.json' do
-    
+    session[:user] = nil
+    Utils.json { |j| j.success true }
   end
 end
