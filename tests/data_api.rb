5ac17bba86bb4f675a0c8771c48a70432b08937b
Alex Wilson <nikosai@gmail.com>
2011-05-19 16:04:04 +1000

    Changing over to using YAML adapter for API tests

--- tests/data_api.rb
+++ tests/data_api.rb
@@ -3,14 +3,13 @@ $:<< File.expand_path("../../lib/", __FILE__)
 require 'config'
 $:.pop
 
-Rota::Config['database']['uri'] = 'sqlite::memory:'
+Rota::Config['database']['uri'] = 'yaml:tests/fixtures/apitests'
 
 require 'rota/model'
 require 'rota/fetcher'
 require 'rubygems'
 
 require 'dm-migrations'
-DataMapper.auto_migrate!
 
 require 'rack/test'
 
@@ -30,20 +29,57 @@ describe 'Data API' do
     RotaApp
   end
   
-  before do
-    @fix = FixtureSet.new('tests/fixtures/data_api_test_data.yml')
-    @fix.save
-  end
-  
-  it 'should return the semester list' do
+  it 'should return the semester list as JSON' do
     get '/semesters.json'
     last_response.should.be.ok
     last_response.headers['Content-Type'].should.match /^text\/javascript/
     body = JSON.parse(last_response.body)
-    body.is_a?(Hash).should.be.true
-    body["semesters"].is_a?(Array).should.be.true
-    ids = body["semesters"].collect { |h| h["id"] }
-    ids.should.include?(6020)
+    body.is_a?(Array).should.be.true
+    ids = body.collect { |h| h["id"] }
     ids.should.include?(6080)
+    ids.should.include?(6110)
+    body.each do |sem|
+      if sem['id'] == 6160
+        sem['is_current?'].should.equal true
+      elsif sem['id'] == 6180
+        sem['start_week'].should.equal 48
+        sem['midsem_week'].should.equal 51
+        sem['finish_week'].should.equal 6
+        sem['is_current?'].should.equal false
+      end
+    end
+  end
+  
+  it 'should return the semester list as XML' do
+    get '/semesters.xml'
+    last_response.should.be.ok
+    last_response.headers['Content-Type'].should.match /^text\/xml/
+    body = last_response.body
+    # verify the xml here
+  end
+  
+  it 'should return details of an individual semester' do
+    get '/semester/6160.json'
+    last_response.should.be.ok
+    last_response.headers['Content-Type'].should.match /^text\/javascript/
+    sem = JSON.parse(last_response.body)
+    sem.is_a?(Hash).should.be.true
+    sem['start_week'].should.equal 30
+    sem['midsem_week'].should.equal 39
+    sem['finish_week'].should.equal 43
+    sem['is_current?'].should.equal true
+  end
+  
+  it 'should return a list of offerings in a semester' do
+    get '/semester/6120/offerings.json'
+    last_response.should.be.ok
+    last_response.headers['Content-Type'].should.match /^text\/javascript/
+    sem = JSON.parse(last_response.body)
+    sem.is_a?(Hash).should.be.true
+    sem['id'].should.equal 6120
+    o = sem['offerings']
+    o.is_a?(Array).should.be.true
+    o.size.should.equal 1
+    o[0]['course']['code'].should.equal "AGRC1906C"
   end
 end
