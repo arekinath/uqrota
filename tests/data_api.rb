dfa551db24837995c752c25e3f7495f966cc5bcc
Alex Wilson <nikosai@gmail.com>
2011-05-22 23:50:31 +1000

    Adjusting API tests for new JSON formats

--- tests/data_api.rb
+++ tests/data_api.rb
@@ -40,12 +40,12 @@ describe 'Data API' do
     ids.should.include?(6110)
     body.each do |sem|
       if sem['id'] == 6160
-        sem['is_current?'].should.equal true
+        sem['current'].should.equal true
       elsif sem['id'] == 6180
         sem['start_week'].should.equal 48
         sem['midsem_week'].should.equal 51
         sem['finish_week'].should.equal 6
-        sem['is_current?'].should.equal false
+        sem['current'].should.equal false
       end
     end
   end
@@ -67,19 +67,16 @@ describe 'Data API' do
     sem['start_week'].should.equal 30
     sem['midsem_week'].should.equal 39
     sem['finish_week'].should.equal 43
-    sem['is_current?'].should.equal true
+    sem['current'].should.equal true
   end
   
   it 'should return a list of offerings in a semester' do
     get '/semester/6120/offerings.json'
     last_response.should.be.ok
     last_response.headers['Content-Type'].should.match /^text\/javascript/
-    sem = JSON.parse(last_response.body)
-    sem.is_a?(Hash).should.be.true
-    sem['id'].should.equal 6120
-    o = sem['offerings']
-    o.is_a?(Array).should.be.true
-    o.size.should.equal 1
-    o[0]['course']['code'].should.equal "AGRC1906C"
+    lst = JSON.parse(last_response.body)
+    lst.is_a?(Array).should.be.true
+    lst.size.should.equal 1
+    lst[0]['course']['code'].should.equal "AGRC1906C"
   end
 end
