5e9bfac201302e9640cbdc893e02801820e91a9b
Alex Wilson <nikosai@gmail.com>
2011-06-07 09:54:15 +1000

    Splitting and operator into andor and orand to control order

--- lib/webapi/common.rb
+++ lib/webapi/common.rb
@@ -11,23 +11,30 @@ class FindConditions
   def do_query_level(hash)
     or_child = nil
     and_child = nil
+    and_first = true
     conds = {}
     hash.each do |k,v|
       if k == 'or'
         or_child = v
-      elsif k == 'and'
+      elsif k == 'and' or k == 'andor'
         and_child = v
+      elsif k == 'orand'
+        and_child = v
+        and_first = false
       else
         kv_pair(conds, k, v)
       end
     end
     
     results = @klass.all(conds)
-    if and_child
+    if and_child and and_first
       results = results & do_query_level(and_child)
     end
     if or_child
       results = results + do_query_level(or_child)
+    end
+    if and_child and not and_first
+      results = results & do_query_level(and_child)
     end
     
     return results
