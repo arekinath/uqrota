1039535ee78bb41c2255aa160692d75f55a0ca7b
Alex Wilson <alex@cooperi.net>
2012-02-17 05:49:15 +0000

    Add commandline port specification to rotad.rb

--- rotad.rb
+++ rotad.rb
@@ -3,6 +3,12 @@
 require File.expand_path('../lib/config', __FILE__)
 require 'app'
 
+class RotaApp 
+  configure do
+    set :port, ARGV[0] ? ARGV[0].to_i : 4567 
+  end
+end
+
 if __FILE__ == $0
   RotaApp.run!
 end
